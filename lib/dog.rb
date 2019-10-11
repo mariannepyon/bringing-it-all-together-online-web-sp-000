21845584c2f6a9ecd26e9551024bd7b0c1a82be9
Marianne Pyon <mariannepyon@gmail.com>
2019-10-11 04:10:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   attr_accessor :id, :name, :breed
 
   def initialize(attributes)
-    id: nil, name:, breed:
+    #id: nil, name:, breed:
     attributes.each {|key, value| self.send(("#{key}="), value)}
     self.id ||= nil
   end
